c33251510d226689516fddfb80e98e4156614caa
MysticVoice <fredrimv@hotmail.com>
2015-04-17 10:31:57 +0200

    La til fleire felt i subklassene til SubTrack

--- Assignment 5 kode/News.java
+++ Assignment 5 kode/News.java
@@ -7,11 +7,13 @@
  */
 public class News extends SubTrack
 {
+    String summary;
     /**
      * Constructor for objects of class News
      */
-    public News(String title, long min, long sec)
+    public News(String title, long min, long sec, String summary)
     {
         super(title,min,sec);
+        this.summary = summary;
     }
 }
