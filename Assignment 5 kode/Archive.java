bb8fec1a3c601191de3bdd1c595f3bcda8c163a8
MysticVoice <fredrimv@hotmail.com>
2015-04-30 11:50:43 +0200

    stff

--- Assignment 5 kode/Archive.java
+++ Assignment 5 kode/Archive.java
@@ -59,6 +59,8 @@ public class Archive
             break;
             
             case 3:
+            clear();
+            
             break;
             
             case 4:
@@ -84,6 +86,8 @@ public class Archive
     public Medium searchArchive()
     {
         String input =  parser.getStringidi();
+        System.out.println(tellUsWhatToDo());
+        
         ArchiveList cdArchive = register.getArchive(0);
         ArchiveList tapeArchive = register.getArchive(1);
         Medium medium = null;
@@ -97,7 +101,11 @@ public class Archive
         {
             medium = medium2;
         }
-        System.out.print(medium.getLongDescription());
+        if(medium != null)
+        {System.out.print(medium.getLongDescription());}
+        else
+        {System.out.print("No results.");}
+        System.out.println("");
         return medium;
         
     }
@@ -132,6 +140,7 @@ public class Archive
     public Tracks searchByTitle()
     {
         String input =  parser.getStringidi();
+        System.out.println("To edit a track type: Edit.");
         activeTracks=new Tracks();
         int stuffs = 0;
         for(int something = 0; something < allMediums.getSize(); something++)
@@ -145,21 +154,25 @@ public class Archive
                 else if(track.getTitle().toLowerCase().contains(input))
                 {
                     activeTracks.addTrack(track);
-                    stuffs++;
+                    
                     System.out.println(stuffs+". "+track.getLongDescription());
+                    stuffs++;
                 }
             }
         }
-        
+        if(activeTracks.getSize() == 0)
+        {System.out.print("No results.");}
+        System.out.println("");
         return activeTracks;
     }
     
-        /**
+    /**
      * Searches for cd with an artist string that contains the input string
      */
     public Tracks searchByArtist()
     {
         String input =  parser.getStringidi();
+        System.out.println("To edit a track type: Edit (index).");
         activeTracks=new Tracks();
         int stuffs= 0;
         for(int something = 0; something < allMediums.getSize(); something++)
@@ -177,8 +190,9 @@ public class Archive
                         else if(((FileOnHDD)track).getArtist().toLowerCase().contains(input))
                         {
                             activeTracks.addTrack(track);
+                            
+                            System.out.println(stuffs+". "+track.getLongDescription());
                             stuffs++;
-                            System.out.print(stuffs+". "+track.getLongDescription());
                         }
                     }
                 }
@@ -188,13 +202,59 @@ public class Archive
                     if(((MusicTrack)track).getArtist().toLowerCase().contains(input))
                         {
                             activeTracks.addTrack(track);
+                            
+                            System.out.println(stuffs+". "+track.getLongDescription());
                             stuffs++;
-                            System.out.print(stuffs+". "+track.getLongDescription());
                         }
                 }
             }
         }
-        
+        if(activeTracks.getSize() == 0)
+        {System.out.print("No results.");}
+        System.out.println("");
+        return activeTracks;
+    }
+    
+    /**
+     * Searches for cd with an artist string that contains the input string
+     */
+    public Tracks listMusic()
+    {
+        activeTracks=new Tracks();
+        int stuffs= 0;
+        for(int something = 0; something < allMediums.getSize(); something++)
+        {
+            Medium medium=allMediums.getMedium(something);
+            for(int something2 = 0; something2 < medium.getSize(); something2++)
+            {
+                Track track = medium.getTrack(something2);
+                
+                if(track instanceof FileOnHDD)
+                {
+                    if (((FileOnHDD)track).getTrack() instanceof MusicTrack)
+                    {
+                        
+                            activeTracks.addTrack(track);
+                            
+                            System.out.println(stuffs+". "+track.getLongDescription());
+                            stuffs++;
+                        
+                    }
+                }
+                else if(track instanceof MusicTrack)
+                {
+                    
+                            activeTracks.addTrack(track);
+                            
+                            System.out.println(stuffs+". "+track.getLongDescription());
+                            stuffs++;
+                        
+                }
+            }
+        }
+        if(activeTracks.getSize() == 0)
+        {System.out.print("No results.");}
+        System.out.println("");
         return activeTracks;
     }
     
@@ -203,9 +263,20 @@ public class Archive
         String menuString = 
         "\n"+"--- Main Menu ---" + "\n"
         +"1. Search" + "\n"
-        +"2. list" + "\n"
-        +"3. medium" +"\n"
-        +"4. quit" + "\n";
+        +"2. List" + "\n"
+        +"3. Add Medium" +"\n"
+        +"4. Quit" + "\n";
+        System.out.print(menuString);
+    }
+    
+    private void printAddMenu()
+    {
+        String menuString = 
+        "\n"+"--- Main Menu ---" + "\n"
+        +"1. Add CD" + "\n"
+        +"2. Add Tape" + "\n"
+        +"3. Add Harddisk" +"\n"
+        +"4. Return" + "\n";
         System.out.print(menuString);
     }
     
@@ -215,7 +286,8 @@ public class Archive
         "--- Search ---" + "\n"
         + "1. Search by archive number" + "\n"
         + "2. Search by title" + "\n"
-        + "3. Search by artist" + "\n";
+        + "3. Search by artist" + "\n"
+        + "4. Return to Main Menu" + "\n";
         System.out.print(menuString);
         
         searchChoise();
@@ -223,7 +295,7 @@ public class Archive
     
     public void searchChoise()
     {
-        int key = parser.getUserMenuSelection(3);
+        int key = parser.getUserMenuSelection(4);
         switch(key)
         {
             case 1:
@@ -233,18 +305,91 @@ public class Archive
             break;
             case 2:
             clear();
+            System.out.println("Search Track by Title.");
             searchByTitle();
+            break;
+            case 3:
+            clear();
+            System.out.println("Search Track by Artist.");
+            searchByArtist();
+            break;
+            case 4:
+            clear();
             break;
         }
     }
     
-    public void printList()
+     public void listChoise()
     {
-        String topThingie = "We found " + allMediums.getSize() + " mediums." + "\n"
-        +"To edit an item type : Edit (index)" + "\n";
-        String mainString = allMediums.getMediumPrintString();
+        int key = parser.getUserMenuSelection(6);
+        Mediums harddisks = register.getMediums(1);
+        Mediums cds = register.getMediums(0);
+        Mediums tapes = register.getMediums(2);
+        switch(key)
+        {
+            case 1:
+            clear();
+            System.out.println(tellUsWhatToDo() + "List of all media and tracks");
+            
+            list(allMediums);
+            break;
+            case 2:
+            clear();
+            System.out.println(tellUsWhatToDo()+"List of CDs.");
+            list(cds);
+            break;
+            
+            case 3:
+            clear();
+            System.out.println(tellUsWhatToDo()+"List of Tapes.");
+            list(tapes);
+            break;
+            case 4:
+            clear();
+            System.out.println(tellUsWhatToDo()+"List of Harddisks.");
+            list(harddisks);
+            break;
+            
+            case 5:
+            clear();
+            System.out.println(tellUsWhatToDo()+"List of Music.");
+            listMusic();
+            break;
+            
+            case 6:
+            clear();
+            break;
+
+        }
+    }
+    
+    private void editOrAdd()
+    {
+        int maxSize = activeMediums.getSize();
+        String stringy = parser.getStringidi();
+        
+        
+    }
+    
+    public String tellUsWhatToDo()
+    {
+        String resultString = "To edit an item type: Edit" + "\n"
+        +"To add a track type: Add" + "\n"
+        +"Anything else will return you to Main Menu" + "\n";
+        return resultString;
+    }
+    
+    public void list(Mediums mediums)
+    {
+        
+        String topThingie = "We found " + mediums.getSize() + " mediums." + "\n";
+        
+        
+        String mainString = mediums.getMediumPrintString();
         System.out.print(topThingie);
         System.out.print(mainString);
+        activeMediums = mediums;
+        editOrAdd();
     }
     
     
@@ -256,8 +401,10 @@ public class Archive
         + "2. List CDs" + "\n"
         + "3. List Tapes" + "\n"
         + "4. List HDDs" + "\n"
-        + "5. List Music" + "\n";
+        + "5. List Music" + "\n"
+        + "6. Return to Main Menu" + "\n";
         System.out.print(returnString);
+        listChoise();
     }
     
     public final static void clear()
