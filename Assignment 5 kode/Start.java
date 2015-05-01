edf68a36972d5a5afa707e1f1e8b26042860986c
Fredrik Mikael Valderhaug <Fredrimv@hotmail.com>
2015-05-01 15:43:53 +0200

    La til start klasse

---
+++ Assignment 5 kode/Start.java
@@ -0,0 +1,20 @@
+
+/**
+ * Write a description of class Start here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Start
+{
+    /**
+     * Constructor for objects of class Start
+     */
+    public Start()
+    {
+    }
+
+    public static void main(String[] args) {
+		Archive archive = new Archive();
+	   }
+}
